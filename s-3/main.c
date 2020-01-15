a9122faa6ec437de363fc3ec9f43a6866c9caae9
satackey <21271711+satackey@users.noreply.github.com>
2020-01-15 14:27:00 +0900

    できた

---
+++ s-3/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+short getRandPointOnCircle(void);
+
+
+int main(void) {
+    int n;
+    printf("nを入力: ");
+    scanf("%d", &n);
+
+    int count = 0;
+    for (int i = 0; i < n; ++i) {
+        short onCircle = getRandPointOnCircle();
+        if (onCircle) {
+            ++count;
+        }
+    }
+
+    double pi = (double)count * 4 / n;
+    printf("%f", pi);
+
+    return 0;
+}
+
+short getRandPointOnCircle(void) {
+    double x = (double)rand() / RAND_MAX;
+    double y = (double)rand() / RAND_MAX;
+    double hypotenuse = sqrt(x*x + y*y);
+
+    if (hypotenuse <= 1) {
+        return 1;
+    }
+
+    return 0;
+}
